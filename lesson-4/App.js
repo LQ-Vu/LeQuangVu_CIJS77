8119bb74232d194144cf09053f48668169bcc63d
LQ-Vu <117506578+LQ-Vu@users.noreply.github.com>
2022-11-05 20:50:00 +0700

    lesson-4

---
+++ lesson-4/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  function clickNext(){
+
+  }
+  return (
+    <div className="App">
+    <button onClick={clickNext}>Next</button>
+    <div className="list-item">
+        <div className="item1"></div>
+        <div className="item2"></div>
+        <div className="item3"></div>
+    </div>
+
+    </div>
+  );
+}
+
+export default App;
