a6d03cb5de0c962e26bdc2d065b94fd7d0c19c42
LQ-Vu <117506578+LQ-Vu@users.noreply.github.com>
2022-11-06 08:12:49 +0700

    big-assignment-1

---
+++ big-assignment-1/src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import './index.css'
+function Header(){
+    return(
+        <div className="Header">
+            <button className="addBtn">ADD NEW EXPENSE</button>
+        </div>
+    )
+}
+export default Header
