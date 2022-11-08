a7ee308e3e2f65a3b1d692a980905d83bc94d905
LQ-Vu <117506578+LQ-Vu@users.noreply.github.com>
2022-11-08 17:19:43 +0700

    ex4

---
+++ lesson-1/ex4/src/components/TodoList/index.js
@@ -0,0 +1,13 @@
+import  TodoItem from "../TodoItem"
+function TodoList(){
+    return(
+        <div>
+        <TodoItem optionname="le"/>
+        <TodoItem optionname="quang"/>
+        <TodoItem optionname="vu"/>
+        </div>
+        
+    );
+}
+
+export default TodoList;
